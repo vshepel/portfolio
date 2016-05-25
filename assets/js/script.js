0bcf76e07fe3719b01ae32f6e154c4534b6bbc89
Vladimir <web.shepel@gmail.com>
2016-05-26 01:51:08 +0300

    portfolio-station redesign

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,84 +13,11 @@ $(document).ready(function() {
     }
   }
 
-  $('#intro a[href*="#"]:not([href="#"])').click(scroll);
-
-  if (event.which == 40) {
-    var station = $(".stations > div a[href*='works']"),
-      train = station.parent().find('img')
-
-    train.queue(a);
-    return false;
-  }
-
-  $('.stations > div').hover(
-    function() {
-      var img = $(this).find('img')
-      img.removeClass('bounceOutLeft').addClass('bounceInLeft');
-    },
-    function() {
-      var img = $(this).find('img')
-      img.addClass('bounceOutLeft').removeClass('bounceInLeft');
-      setTimeout(function() {
-        img.removeClass('bounceOutLeft');
-      }, 500);
-    }
-  );
-
-  $(document).keydown(function(event) {
-
-    var a = function() {
-      var href = station.attr('href');
-      if ($(this).hasClass('bounceInLeft')) {
-        $(this).addClass('slideOutRight');
-        $('#white').addClass('visable');
-        setTimeout(function() {
-          window.location = href
-        }, 1000);
-      } else {
-        $(this).addClass('bounceInLeft');
-        setTimeout(function() {
-          train.addClass('slideOutRight');
-          $('#white').addClass('visable');
-        }, 1000);
-        setTimeout(function() {
-          window.location = href
-        }, 2000);
-      }
-    }
-
-    if (event.which == 87) {
-      var station = $(".stations > div a[href*='works']"),
-        train = station.parent().find('img')
-
-      train.queue(a);
-      return false;
-    }
-
-    if (event.which == 72) {
-      var station = $(".stations > div a[href*='help']"),
-        train = station.parent().find('img')
-
-      train.queue(a);
-      return false;
-    }
-
-    if (event.which == 67) {
-      var station = $(".stations > div a[href*='contact']"),
-        train = station.parent().find('img')
-
-      train.queue(a);
-      return false;
-    }
-
-  });
-
-  $('.stations > div a').on('click', function() {
-    var train = $(this).parent().find('img'),
-      href = $(this).attr('href')
+  $('#route a').on('click', function() {
+    var href = $(this).attr('href')
 
     $('#white').addClass('fadeIn');
-    train.addClass('slideOutRight');
+    $('#route .train').addClass('out');
     setTimeout(function() {
       window.location = href
     }, 1000);
